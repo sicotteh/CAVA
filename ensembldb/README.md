ae80e510551611002027d2f04da160ba09dde849
Steven-N-Hart <steven.n.hart@gmail.com>
2020-09-25 10:40:58 -0500

    Standardized VERSION Automatic hg19 conversion Verified targeted and non-targeted conversion

--- ensembldb/README.md
+++ ensembldb/README.md
@@ -1,2 +1,26 @@
 # Preparing a transcript database
 
+```
+python3 CAVA/bin/EnsemblDB.py -h
+
+Usage: CAVA-1.2.5/ensembl_db <options>
+
+CAVA ensembl_db v1.2.5
+
+Options:
+  --version             show program's version number and exit
+  -h, --help            show this help message and exit
+  -i INPUT, --input=INPUT
+                        Input filename (list of ENST IDs)
+  -o OUTPUT, --output=OUTPUT
+                        Output filename prefix
+  -e ENSEMBL, --ensembl=ENSEMBL
+                        Ensembl release version.py
+  --no_hg19             Set this to skip hg19 builds (will build by default)
+  -s, --select          Select transcript for each gene [default: False]
+
+Example usage: CAVA/ensembl_db -e 75 -o ensembl_db_75
+Note: by default, hg19 will be created using crossmap
+Version: 1.2.5
+
+```
--- test.sh
+++ test.sh
@@ -8,31 +8,41 @@ set -e
 set -o pipefail
 
 # Set up
-
-if [ ! -f test/tmp.input.vcf.gz ]
+if [ ! -f test/tmp.GRCh38.fa ]
 then
-    curl ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606/VCF/common_all_20180418.vcf.gz \
-        | gunzip \
-        | awk '$1 ~ "^#" || NR % 1000 == 0' \
-        | gzip > test/tmp.input.vcf.gz
+    curl --cipher 'DEFAULT:!DH' http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz | gunzip > test/tmp.GRCh38.fa
+    samtools faidx test/tmp.GRCh38.fa
 fi
 
-if [ ! -f test/tmp.hg38.fa ]
+if [ ! -f test/tmp.hg19.fa ]
 then
-    curl http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz | gunzip > test/tmp.hg38.fa
-    samtools faidx test/tmp.hg38.fa
+    curl --cipher 'DEFAULT:!DH' https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz | gunzip > test/tmp.hg19.fa
+    samtools faidx test/tmp.hg19.fa
 fi
+
+# Install program
+python3 setup.py install
 
 # Test: prepare database
-./ensembl_db -e 75 -o test/tmp.db
+python3 bin/EnsemblDB.py -e 101 -o ENST101_test
+python3 bin/EnsemblDB.py -e 101 -o ENST101_test_small -i test/CustomTX.txt
+python3 bin/EnsemblDB.py -e 75 -o ENST75_test
+python3 bin/EnsemblDB.py -e 75 -o ENST75_test_small -i test/CustomTX.txt
 
-# Lengths in bp not kb:
-gunzip -c test/tmp.db.gz | grep -F '+/919bp/1/918bp/305' > /dev/null
 
 # Test: annotate
-./cava -i test/tmp.input.vcf.gz -o test/tmp. -c test/CAVA_config.txt -t 8
+# build 38 test full
+python3 bin/CAVA.py -i test/test.GRCh38.vcf -o test/tmp1 -c test/CAVA_config_1.txt
+# build 38 test small
+python3 bin/CAVA.py -i test/test.GRCh38.vcf -o test/tmp2 -c test/CAVA_config_2.txt
+# build 37 test full
+python3 bin/CAVA.py -i test/test.GRCh37.vcf -o test/tmp3 -c test/CAVA_config_3.txt
+# build 37 test small
+python3 bin/CAVA.py -i test/test.GRCh37.vcf -o test/tmp4 -c test/CAVA_config_4.txt
+
+tail -n2 test/tmp*vcf # None should have '.' in CSN
 
 # Tear down
 # =========
 
-rm test/tmp.*
+#rm test/tmp.*
