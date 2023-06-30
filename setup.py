cfd404c17f04ba198be8a27a33486dd7c4e1222d
Sicotte.Hugues@mayo.edu <m037385@mforgehn2.mayo.edu>
2023-06-30 15:32:38 -0500

    change build process to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,24 +1,5 @@
 from setuptools import setup
 import os
-with open(os.path.join(os.path.dirname(__file__), 'cava', 'VERSION')) as version_file:
-    version = version_file.read().strip()
 
-with open('requirements.txt') as requires_file:
-    requires = requires_file.read().split('\n')
-
-setup(
-    name='CAVA',
-    python_requires=">=3.6",
-    version=version,
-    description='CAVA (Clinical Annotation of VAriants)',
-    url='https://github.com/Steven-N-Hart/CAVA',
-    author='Steven-N-Hart, Hugues-Sicotte',
-    author_email='hart.steven@mayo.edu, sicotte.hugues@mayo.edu',
-    license='MIT',
-    packages=['cava', 'cava.utils', 'cava.ensembldb'],
-    install_requires=requires,
-    zip_safe=False,
-    include_package_data=True,
-    test_suite='tests',
-    tests_require=['wget',"pycurl >= 7.45.1"],
-)
+if __name__ == '__main__':
+    setup()
